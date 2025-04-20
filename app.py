271094ebeace36c7d40c4d223a80c2a2eec992d6
Luis FontuMi <fontumiit2@MacBook-Air--Luis-FontuMi.local>
2025-04-20 18:51:48 -0500

    Moviendo archivos del backend a la raíz del proyecto

--- app.py
+++ app.py
@@ -167,4 +167,5 @@ def borrar_historial():
     return render_template("historial.html", registros=[])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Usa el puerto asignado por Render o 5000 por defecto
+    app.run(host="0.0.0.0", port=port)
